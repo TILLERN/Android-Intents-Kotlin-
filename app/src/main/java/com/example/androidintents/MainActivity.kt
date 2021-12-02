928143504b310e0aace2a60908887238c23d40d2
TILLERN <brysonminodi@google.com>
2021-12-02 16:16:48 +0300

    button id not visible

--- app/src/main/java/com/example/androidintents/MainActivity.kt
+++ app/src/main/java/com/example/androidintents/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.androidintents
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnOpenNewActivity.setOnClickListener{
+            Intent(this, NewActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
