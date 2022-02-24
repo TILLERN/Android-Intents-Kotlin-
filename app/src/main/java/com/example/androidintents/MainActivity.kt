5cf2b360a95627df987f780643c114292da7c9f5
TILLERN <brysonminodi@google.com>
2022-02-24 18:49:41 +0300

    done

--- app/src/main/java/com/example/androidintents/MainActivity.kt
+++ app/src/main/java/com/example/androidintents/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.androidintents
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnOpenNewActivity.setOnClickListener{
+
+        val mybutton = findViewById<Button>(R.id.btnOpenNewActivity)
+
+        mybutton.setOnClickListener{
             Intent(this, NewActivity::class.java).also {
                 startActivity(it)
             }
--- app/src/main/java/com/example/androidintents/NewActivity.kt
+++ app/src/main/java/com/example/androidintents/NewActivity.kt
@@ -2,14 +2,18 @@ package com.example.androidintents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class NewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new)
 
-        btnBack.setOnClickListener{
+        val backButton = findViewById<Button>(R.id.btnBack)
+
+        backButton.setOnClickListener{
             finish()
         }
     }
 }
+
